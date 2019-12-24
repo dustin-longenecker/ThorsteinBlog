4f2ff14fd1f79933da141daa5fecd635479e8a0d
dustin-longenecker <dl-dev@outlook.com>
2019-12-24 11:47:13 -0700

    initial commit

---
+++ SkillCrush/front-page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for the homepage
+ *
+ * @package WordPress
+ * @subpackage Skillcrush_Starter
+ * @since Skillcrush Starter 2.0
+ */
+
+get_header(); ?>
+
+<section class="home-page">
+	<div class="main-content">
+		<div class="content">
+			<?php while ( have_posts() ): the_post(); ?>
+				<h1><?php the_title(); ?></h1>
+				<?php the_content(); ?>
+				<div class="social-btns">  <!-- populate or remove as many of these as you want -->
+					<a href="https://twitter.com/thorsteincorner" class="soc-icon tw"><span class="screen-reader-text">Twitter</span></a>
+					<a href="https://www.minds.com/d4rkw01f/" class="soc-icon ln"><span class="screen-reader-text">Minds</span></a>
+
+					<a href="https://dribbble.com/search/programming" class="soc-icon db"><span class="screen-reader-text">Dribble</span></a>
+				</div>
+				<a href="<?php echo site_url('/blog/'); ?>" class="btn">View My Blog</a>
+			<?php endwhile; ?>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
